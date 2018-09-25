09ab3e4c52cc4c0645f2c28a5cbc05dcccee6bc9
INGENIERIA <ingenieria@SALAMAC06.local>
2018-09-25 13:41:27 -0500

    implements cube health

---
+++ Assets/Scripts/Resistance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resistance : MonoBehaviour {
+
+
+	public int resistance;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	public void shooted(){
+		resistance--;
+
+		if (resistance <= 0) {
+			Destroy (transform.gameObject);
+		}
+
+	}
+}
